630a3f7a1f608e2228ffaea67e3af97b2652415b
fairfriend92 <fairfriend92@gmail.com>
2016-12-09 13:36:56 +0000

    Pulizia file locali - non importante

---
+++ MyApplication/app/src/main/cpp/native_method.h
@@ -0,0 +1,36 @@
+//
+// Created by rodolfo on 08/12/16.
+//
+
+#ifndef MYAPPLICATION_NATIVE_METHOD_H_H
+#define MYAPPLICATION_NATIVE_METHOD_H_H
+
+#endif //MYAPPLICATION_NATIVE_METHOD_H_H
+
+#define SAMPLING_RATE 0.1
+#define SYNAPSE_FILTER_ORDER 64
+#define EXC_SYNAPSE_TIME_SCALE 1
+#define NUMBER_OF_EXC_SYNAPSES 4
+
+#include <android/log.h>
+#include <jni.h>
+#include <math.h>
+#include "CL/cl.h"
+#include "common.h"
+
+struct OpenCLObject {
+    // OpenCL implementation
+    cl_context context = 0;
+    cl_command_queue commandQueue = 0;
+    cl_program program = 0;
+    cl_device_id device = 0;
+    cl_kernel kernel = 0;
+    cl_int errorNumber = 0;
+    int numberOfMemoryObjects = 3;
+    cl_mem memoryObjects[3] = {0, 0, 0};
+    cl_uint integerVectorWidth = 0;
+
+    // Pointers to the memory buffers
+    cl_float *excSynapseCoeff;
+    cl_bool *excSynapseInput;
+};
