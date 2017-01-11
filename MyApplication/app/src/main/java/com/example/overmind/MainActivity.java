8b055ce95bbaabb43c61cfac98c9615813b5f306
fairfriend92 <fairfriend92@gmail.com>
2017-01-11 21:44:04 +0000

    Aggiunto codice per identificare vendor GPU in modo da caricare dinamicamente la libreria OpenCL appropriata

--- MyApplication/app/src/main/java/com/example/overmind/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/overmind/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.overmind;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +15,40 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 public class MainActivity extends AppCompatActivity {
+
+    private class MyGLRenderer implements GLSurfaceView.Renderer {
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            // Get various GPU and GL information
+            Log.d("GL info", "gl renderer: "+gl.glGetString(GL10.GL_RENDERER));
+            Log.d("GL info", "gl vendor: "+gl.glGetString(GL10.GL_VENDOR));
+            Log.d("GL info", "gl version: "+gl.glGetString(GL10.GL_VERSION));
+            Log.d("GL info", "gl extensions: "+gl.glGetString(GL10.GL_EXTENSIONS));
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("VENDOR", gl.glGetString(GL10.GL_VENDOR));
+            editor.apply();
+
+            // Set the background frame color
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            // Redraw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+    }
 
     class MyGLSurfaceView extends GLSurfaceView {
         public MyGLRenderer mRenderer;
@@ -30,38 +66,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //MyGLSurfaceView mGlSurfaceView = new MyGLSurfaceView(this);
-
-        GLSurfaceView mGlSurfaceView = new GLSurfaceView(this);
-        MyGLRenderer mGLRenderer = new MyGLRenderer();
-        mGlSurfaceView.setRenderer(mGLRenderer);
-
+        prefs = this.getSharedPreferences("GPUinfo", Context.MODE_PRIVATE);
+        MyGLSurfaceView mGlSurfaceView = new MyGLSurfaceView(this);
         setContentView(mGlSurfaceView);
 
-        //while (mGlSurfaceView.mRenderer.mVendor == null) { }
-        //setContentView(R.layout.activity_main);
+        /*
+        final Intent intent = new Intent(this, MainActivity.class);
+        new CountDownTimer(3000, 9999)
+        {
+            public void onTick(long millisUntilFinished) {
+                // Not used
+            }
+            public void onFinish() {
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+        */
 
-        Log.d("robin", " " +  mGLRenderer.mVendor);
+        setContentView(R.layout.activity_main);
+        loadGLLibrary();
     }
 
-    static {
-        try {
-            System.load("libGLES_mali.so");
+
+    public void loadGLLibrary () {
+        SharedPreferences prefs =getSharedPreferences("GPUinfo",Context.MODE_PRIVATE);
+        String vendor = prefs.getString("VENDOR", null);
+        assert vendor != null;
+        switch (vendor) {
+            case "ARM":
+                try {
+                    System.load("libGLES_mali.so");
+                }
+                catch (UnsatisfiedLinkError linkError) {
+                    Log.e("Unsatisfied link", "libGLES_mali.so not found");
+                }
+                break;
+            default:
+                try {
+                    System.load("libOpenCL.so");
+                } catch (UnsatisfiedLinkError linkError) {
+                    Log.e("Unsatisfied link", "libGLES_mali.so not found");
+                }
         }
-        catch (UnsatisfiedLinkError linkError) {
-            Log.e("Unsatisfied link", "libGLES_mali.so not found");
-        }
-        /*
-        try {
-            System.load("libOpenCL.so");
-        } catch (UnsatisfiedLinkError linkError) {
-            Log.e("Unsatisfied link", "libGLES_mali.so not found");
-        }
-        */
     }
 
     static {
@@ -95,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
             // Put the string holding the kernel in the simulation Intent
             simulationIntent.putExtra("Kernel", synapseKernelVec4);
             // Start the service
-            //this.startService(simulationIntent);
+            this.startService(simulationIntent);
             simulationIsStarted = true;
         }
     }
